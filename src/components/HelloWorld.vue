5fb3642206c54755577d337832f2d768e2139f30
Jungmo Kang <moya.kang@gmail.com>
2021-05-11 08:57:08 +0900

    initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,44 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div>
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+    </div>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Input: Array of Number. ( Number >=0 &amp;&amp; Number &lt; 1000). Array's length &lt; 100,000
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p>
+      output: biggist number of combined Input.
+    </p>
+    <blockquote>
+      i.g ) input is [6,1,40], combined Input can be '6140', '6401', '1640', '4061', '4016'm so the biggist number is '6401'. output must be '6401'
+    </blockquote>
+    <p>Please generate input arrays when you click 'run' button. and get out number.</p>
+    <button @click="run">Run</button>
+    <p>inupt: {{input}}</p>
+    <p>result: {{result}}</p>
   </div>
 </template>
 
+
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      input: [],
+      result: ''
+    }
+  },
+   methods: {
+    run () {
+    }
+
   }
+
 }
 </script>
 
@@ -54,5 +57,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.hello {
+  text-align: center;
 }
 </style>
